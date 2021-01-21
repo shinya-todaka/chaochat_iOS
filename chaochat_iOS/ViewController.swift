570b9d7b81898adf8117c789b1fc7213b038c8c9
Shinya Todaka <tdk0105.strawberry@gmail.com>
2021-01-21 15:56:23 +0900

    Initial Commit

---
+++ chaochat_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chaochat_iOS
+//
+//  Created by 戸高新也 on 2021/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
